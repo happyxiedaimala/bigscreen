bc6679c978747032fd16c4fedd657181c1f884f2
yyfs <1725350259@qq.com>
2022-10-14 16:08:48 +0800

    实验2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,13 @@ import "./home.scss";
 import image from "../images/header.png";
 import { Chart1 } from "../components/chart-1";
 import { Chart2 } from "../components/chart-2";
+import { Chart3 } from "../components/chart-3";
+import { Chart4 } from "../components/chart-4";
+import { Chart5 } from "../components/chart-5";
+import { Chart6 } from "../components/chart-6";
+import { Chart7 } from "../components/chart-7";
+import { Chart8 } from "../components/chart-8";
+import { Chart9 } from "../components/chart-9";
 
 export const Home = () => {
 	return (
@@ -13,11 +20,27 @@ export const Home = () => {
 					<Chart1 />
 					<Chart2 />
 				</section>
-				<section className="bordered section2"></section>
-				<section className="bordered section3"></section>
-				<section className="bordered section4"></section>
+				<section className="section2">
+					<Chart3 />
+					<Chart4 />
+				</section>
+				<section className="bordered section3">
+					<Chart5 />
+				</section>
+				<section className="section4">
+					<Chart6 />
+					<div className="bordered 年龄段">
+						<h2>犯罪人员年龄段分布</h2>
+						<div className="charts">
+							<Chart7 />
+							<Chart8 />
+							<Chart9 />
+						</div>
+					</div>
+				</section>
 				<section className="bordered section5"></section>
 			</main>
-		</div> //这段语法不是很明白
+			<footer>© Li 2022-2023</footer>
+		</div>
 	);
 };
